86a56ef74206bc1e8c4c545656aff004717d283e
pizza122333 <chy7355@naver.com>
2026-05-18 17:01:59 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(void)
+{
+    // 1. 이미지 로드
+    Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
+    if (src.empty()) {
+        cerr << "Image load failed!" << endl;
+        return -1;
+    }
+
+    // 조건 1: 중심은 9, 주변은 -1인 3x3 필터 마스크 정의
+    float data[] = {
+        -1.0f, -1.0f, -1.0f,
+        -1.0f,  9.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f
+    };
+    Mat sharpen_kernel(3, 3, CV_32FC1, data);
+
+    Mat dst;
+
+    // 조건 2: delta 매개변수를 128에서 -> 0으로 설정
+    filter2D(src, dst, -1, sharpen_kernel, Point(-1, -1), 0);
+
+    // 결과 출력
+    imshow("src", src);
+    imshow("dst", dst);
+    waitKey();
+
+    return 0;
+}
